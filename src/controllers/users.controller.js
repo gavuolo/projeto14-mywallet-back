58d6fd25a65902b575f24dad5eefe80f489dea3f
gavuolo <gabs.vuolo@gmail.com>
2022-11-21 15:29:06 -0400

    crypto password

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,21 +1,41 @@
-import {schemaSignUp} from "../index.js"
-import {userCollection} from "../DataBase/db.js"
+import { schemaSignUp } from "../index.js"
+import { userCollection } from "../DataBase/db.js"
+import bcrypt from "bcrypt"
 
-export async function signUp(req, res){
+export async function signUp(req, res) {
     const user = req.body;
 
     const validation = schemaSignUp.validate(user)
-    if(validation.error){
+    if (validation.error) {
         return res.sendStatus(422);
     }
 
     try {
-        await userCollection.insertOne(user);
+        
+        const userExist = await userCollection.findOne({email: user.email})
+        if(userExist){
+            res.status(409).send({message: "Email já cadastrado"})
+            return
+        }
+        const hashPassword = bcrypt.hashSync(user.password, 10);
+        await userCollection.insertOne({...user, password: hashPassword});
         res.sendStatus(201)
-  
-    }catch(error){
+
+    } catch (error) {
         console.log(error)
         res.sendStatus(500)
     }
 
 }
+
+// export async function signIn(req, res) {
+//     const { email, password } = req.body;
+//     try {
+//         await userCollection.findOne({ email, password })
+//         res.sendStatus(201)
+//     }
+//     catch (error) {
+//         console.log(error)
+//         res.sendStatus(500)
+//     }
+// }
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,5 +1,7 @@
 import { signUp } from "../controllers/users.controller.js";
 import {Router} from "express"
 const router = Router()
+
 router.post("/sign-up", signUp)
+//router.post("/", signIn)
 export default router;
